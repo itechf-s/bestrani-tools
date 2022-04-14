5a13d3a6ca61e4df15c5b88c547eff0d1003aaee
Kaunain Ahmad <kaunain.ahmad2000@gmail.com>
2022-04-14 12:44:31 +0530

    code updation

--- src/components/tool-section/index.tsx
+++ src/components/tool-section/index.tsx
@@ -1,19 +1,46 @@
 import { tw } from 'twind';
-import Preferences from '@/constants/svg/preferences.svg';
-import Play from '@/constants/svg/play.svg';
+import Button from '@/components/button';
 
-const VideoSection = () => (
+const ToolSection = () => (
   <div>
-    <h3 className="p-6 max-w-sm mx-auto bg-red-400">line one</h3>
-    <h1 className={tw(`p-6 max-w-sm mx-auto bg-red-400 rounded-xl`)}>my tool</h1>
-    <input type="text" className={tw(`p-6 max-w-sm mx-auto bg-blue-100 flex rounded-xl items-center space-x-4`)} />
-    <input
-      type="date"
-      name="today"
-      id="today"
-      className={tw(`p-6 max-w-sm mx-auto bg-blue-100	flex rounded-xl items-center space-x-4`)}
-    />
+    <h2 className="text-center-2 pb-4 text-5xl lg:text-7xl font-bold tracking-tight text-gray-900 rounded-xl">
+      Ideal Body Weight Calculator
+    </h2>
+    <div className={tw(`w-full lg:w-1/2 mt-12 lg:mt-0 items-center`)}>
+      <div className={tw(`border border-red-400 rounded py-5 px-4`)}>
+        <h4 className={tw(`font-mono text-sm uppercase text-gray-500 mb-3`)}>Gender</h4>
+        <div className={tw(`flex w-full`)}>
+          <input
+            aria-label="ages"
+            type="radio"
+            className={tw(`border border-gray-300 bg-red-100 min-w-0 w-full rounded text-gray-800 py-2 px-3 mr-2`)}
+            placeholder="Ages 2 - 80"
+          />
+        </div>
+        <h4 className={tw(`font-mono text-sm uppercase text-gray-500 mb-3`)}>Age</h4>
+        <div className={tw(`flex w-full`)}>
+          <input
+            aria-label="ages"
+            type="text"
+            className={tw(`border border-gray-300 bg-red-100 min-w-0 w-full rounded text-gray-800 py-2 px-3 mr-2`)}
+            placeholder="Ages 2 - 80"
+          />
+        </div>
+        <h4 className={tw(`font-mono text-sm uppercase text-gray-500 mb-3`)}>Height</h4>
+        <div className={tw(`flex w-full`)}>
+          <input
+            aria-label="height"
+            type="text"
+            className={tw(`border border-gray-300 bg-red-100 min-w-0 w-full rounded text-gray-800 py-2 px-3 mr-2`)}
+            placeholder="Enter your height in Cms"
+          />
+        </div>
+        <div className={tw(`flex w-full`)}>
+          <Button>Subscribe</Button>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
-export default VideoSection;
+export default ToolSection;
