6dfc4a7a49e3e8bf332c67a60eab0fee87a9c6fa
Kaunain Ahmad <kaunain.ahmad2000@gmail.com>
2022-04-15 16:59:14 +0530

    updated

--- src/components/age-cal/index.tsx
+++ src/components/age-cal/index.tsx
@@ -4,35 +4,38 @@ import { tw } from 'twind';
 
 type Inputs = {
   birthDate : Date,
-  example: string,
-  exampleRequired: string,
+  presentDate : Date
 };
 
 export default function App() {
   const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
 
-  console.log(watch("example"))
+  console.log(watch("birthDate"))
 
   return (
     <div className={tw(`container`)}>
       <form onSubmit={handleSubmit(onSubmit)} className={tw(`border border-gray-300 bg-red-100 min-w-0 w-full rounded text-gray-800 py-2 px-3 mr-2`)} >
-      <input type="date" placeholder="birthDate" {...register("birthDate")} />
+      <label>Birth Date</label>
+      <br />
+      <input type="date" placeholder="birthDate" {...register("birthDate")}
+      className={tw(`text-red-500 py-3 mb-3`)} />
+      <label>Present Date</label>
+      <br />
+      <input type="date" placeholder="presentDate" {...register("presentDate")}
+      className={tw(`text-red-500 py-3 mb-3`)}
+      />
 
-        <input defaultValue="test" {...register("example")} 
-        className={tw(`border border-gray-300 bg-red-100 min-w-0 w-full rounded text-gray-800 py-2 px-3 mr-2`)}
-        />
-        
-        <input {...register("exampleRequired", { required: true })} 
-        className={tw(`border border-gray-300 bg-red-100 min-w-0 w-full rounded text-gray-800 py-2 px-3 mr-2`)}
-        />
-
-        {errors.exampleRequired && <span className={tw(`text-red-500 py-3 mb-3`)}>This field is required</span>}
+        {errors.birthDate && <span className={tw(`text-red-500 py-3 mb-3`)}>This field is required</span>}
         <br/>
-        <button className={tw(`inline-block bg-yellow-500 text-yellow-800 rounded shadow py-2 px-5 text-sm`)}>
-          Submit
+        <button className={tw(`text-uppercase inline-block bg-yellow-500 text-yellow-800 rounded shadow py-2 px-5 text-sm`)}>
+          Calculate
         </button>
       </form>
+      <div>
+Result
+
+      </div>
     </div>
   );
 }
