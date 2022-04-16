7fa3e496f6ca5ebb61a50f822f64039614c76a82
na-ir <hussain07ali@gmail.com>
2022-04-16 14:08:45 +0530

    Age calculator

--- src/components/age-cal/index.tsx
+++ src/components/age-cal/index.tsx
@@ -14,78 +14,88 @@ export default function App() {
   console.log(watch("birthDate"))
 
   return (
-    <div className={tw(`place-self-center`)}>
-    <div className={tw(`container`)}>
-      <form onSubmit={handleSubmit(onSubmit)} className={tw(`border border-gray-800 bg-red-200 min-w-0 rounded text-gray-800 py-6 px-3 mr-2 w-60`)} >
-      <label>SELECT DATE OF BIRTH</label>
+       <div className={tw(`container`)}>
+  
+       <center>
+      <form onSubmit={handleSubmit(onSubmit)} className={tw(`border border-gray-800 block p-6 rounded-lg shadow-lg bg-pink-200 max-w-sm m-6`)} >
+  
+      
+      <label><p class="text-lg text-black font-semibold">SELECT DATE OF BIRTH</p></label>
       <br />
       <input type="date" placeholder="birthDate" defaultValue="2017-10-13" {...register("birthDate", {required: true})}
-      className={tw(`text-red-500 py-3 mb-3`)} />  <br/>
-      <label>AGE AT THE DATE OF</label>
+      className={tw(`bg-gray-100 text-gray-500 py-2 mb-2 w-full`)} />  <br/>
+      <label><p class="text-lg text-black font-semibold">AGE AT THE DATE OF</p></label>
       <br/>
     <input type="date" placeholder="presentDate" defaultValue="2022-03-12" {...register("presentDate")}
-      className={tw(`text-red-500 py-3 mb-3`)}
-      />
+       className={tw(`bg-gray-100 text-gray-500 py-2 mb-2 w-full`)} />  <br/>
+    
 
-        {errors.birthDate && <span className={tw(`text-red-500 py-3 mb-3`)}>This field is required</span>}
+        {errors.birthDate && <span className={tw(`shadow appearance-none border rounded w-full text-red-500 py-3 mb-3`)}>This field is required</span>}
         <br/>
-        <button className={tw(`text-uppercase inline-block bg-yellow-700 text-blue-800 rounded shadow py-2 px-5 text-sm hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2`)}>
+        <center>
+        <button className={tw(`text-uppercase text-black font-semibold inline-block text-blue-800 rounded shadow py-2 px-5 text-sm hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2`)}>
           Calculate
         </button>
+        </center>
+      
       </form>
+      </center>
       <div>
-        </div>
+      
         
-Result
-<form onSubmit={handleSubmit(onSubmit)} className={tw(`border border-gray-800 bg-white-200 min-w-0 rounded text-gray-800 py-6 px-3 mr-2 w-60`)} >
+<center><p class="text-lg text-black font-semibold">Result</p></center>
+
+<center>
+<form onSubmit={handleSubmit(onSubmit)} className={tw(`border border-gray-800 block p-6 rounded-lg shadow-lg bg-pink-200 max-w-sm m-6`)} >
 <div class="space-y-0.5">
-      <p class="text-lg text-black font-semibold">
+      <p class="text-lg text-black font-semibold mb-2">
       Your Age is:
       </p>
-      <p class="text-slate-500 font-medium">
+      <p class="text-blue-500 font-medium mb-2">
       31 years, 3 months, 15 days
       </p>
-      <p class="text-lg text-black font-semibold">
+      <p class="text-lg text-black font-semibold mb-2">
       Your Age in Month is:
       </p>
-      <p class="text-slate-500 font-medium">
+      <p class=" text-blue-500 font-medium mb-2">
       375 months, 2 weeks, 1 days
       </p>
-      <p class="text-lg text-black font-semibold">
+      <p class="text-lg text-black font-semibold mb-2">
       Your Age in Week is:
       </p>
-      <p class="text-slate-500 font-medium">
+      <p class="text-blue-500 font-medium mb-2">
       1632 weeks, 4 days, 11 hours
       </p>
-      <p class="text-lg text-black font-semibold">
+      <p class="text-lg text-black font-semibold mb-2">
       Your Age in Days is:
       </p>
-      <p class="text-slate-500 font-medium">
+      <p class="text-blue-500 font-medium mb-2">
       11428 days, 11 hours, 12 minutes
       </p>
-      <p class="text-lg text-black font-semibold">
+      <p class="text-lg text-black font-semibold mb-2">
       Your Age in Hours is:
       </p>
-      <p class="text-slate-500 font-medium">
+      <p class="text-blue-500 font-medium mb-2">
       274283 hours, 12 minutes, 8 seconds
       </p>
-      <p class="text-lg text-black font-semibold">
+      <p class="text-lg text-black font-semibold mb-2">
       Your Age in Minutes is:
        </p>
-      <p class="text-slate-500 font-medium">
+      <p class="text-blue-500 font-medium mb-2">
       16456992 minutes, and 8 seconds
       </p>
-      <p class="text-lg text-black font-semibold">
+      <p class="text-lg text-black font-semibold mb-2">
       Your Age in Seconds is:
        </p>
-      <p class="text-slate-500 font-medium">
+      <p class="text-blue-500 font-medium mb-2">
       987419528 seconds since your birth
       </p>
-
     </div>
 </form>
+</center>
 
       </div>
     </div>
+    
   );
 }
