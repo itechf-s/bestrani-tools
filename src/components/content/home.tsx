86e65dda6ff6e358aa3222a8abec5680fb2d9f7f
na-ir <hussain07ali@gmail.com>
2022-04-21 14:10:05 +0530

    Calculator description

--- src/components/content/home.tsx
+++ src/components/content/home.tsx
@@ -1,32 +1,48 @@
 
 const home = {
   content: {
-    title: "Love Calculator Test",
-    description: "LoveTeste",
+    title: "Calculator",
+    description: "A Small Electronic Machine Used For Calculating Figures You can do simple or advanced calculations.",
     listItems: [
       {
-        title: `Love Compatibility Hello`,
-        description: `Officia et fugiat mollit qui. Dolor elit aliqua voluptate ipsum
-      excepteur cillum consequat consectetur duis magna qui eu consequat occaecat.
-      Deserunt nisi sit.`,
+        title: `What is Calculator`,
+        description: `It is an electronic device. It makes the difficult calculations so easy and on tips. The machine is fully equipped with the functionality of solving various problems. For example, Addition, subtraction, Multiplication and Division. Above all, it is no more related to these four functions only. More functional and technical devices are available to give a great experience to the user.`,
       },
       {
-        title: `Developers`,
-        description: `Pariatur consectetur laboris exercitation duis laboris.
-      Commodo duis fugiat magna fugiat et ut anim elit. Tempor aute ex qui
-      tempor tempor.`,
+        title: `History of Calculator`,
+        description: `The importance of this helpful tool was known in the early times. At that time, early men use bones, pebbles and other objects to tally the calculations. 
+        But, it was very limited in nature. So, when the first mainframe computers came into existence. It was using vacuum tubes and transistors. That leads to stepping stones to develop an electronic calculator. Therefore, the first electronic calculator was launched by Casio computer company in 1957. followed by a series of models with compact technology. Some leading manufactures opted for this technology to cover this new market. Like Canon, Sony, Toshiba, Olivetti, etc.
+
+`,
       },
       {
-        title: `Product owners`,
-        description: `Ullamco consectetur ipsum eiusmod nisi adipisicing sint anim
-      dolore aute excepteur. Voluptate ea ullamco sunt eu elit qui aliquip.
-      Adipisicing.`,
+        title: `What Are the Different Types of Calculators`,
+        description: `Every calculator can do basic arithmetic, but there are some that are more advanced than others. The different types of calculators are broken down and described below. Basic Calculators, Scientific Calculators, Graphing Calculators, Printing Calculators, Financial Calculators, Online Calculators, Phone Calculators, Mechanical Calculators, Electronic Calculators, Solar Powered Calculators, Touchscreen Calculators, Pocket Calculators, Desktop Calculators, Novelty Calculators`,
+        
       },
       {
-        title: `Product owners ek aur`,
-        description: `Ullamco consectetur ipsum eiusmod nisi adipisicing sint anim
-      dolore aute excepteur. Voluptate ea ullamco sunt eu elit qui aliquip.
-      Adipisicing.`,
+        title: `Advantages of Calculator`,
+        description: `You can solve very tedious and most difficult arithmetic problems. It is very portable and easy to use machine. Moreover, the biggest advantage is calculation is always correct.These points are the calculator advantages.`,
+      },
+
+      {
+        title: `Why do we use calculators`,
+        description: `Calculators are increasingly essential tools, simple and efficient devices with which you can solve more complex operations. But are the advantages they can bring to teaching really so many?`,
+      },
+
+      {
+        title: `Saves time`,
+        description: `When the objective is to develop general mathematical reasoning abilities or research of numerical patterns, the calculator is necessary to avoid spending minutes solving long and tedious operations. Investigating is also a vital part of the knowledge that we sometimes put aside for lack of time!`,
+      },
+
+      {
+        title: `It is necessary to learn its use`,
+        description: `Both in secondary and high school or even in the university, the calculator becomes a necessary asset inevitably; so students will use it more and more frequently. And since it is a daily instrument who better than teachers to teach them how to handle them correctly?`,
+      },
+
+      {
+        title: `They help motivate students`,
+        description: `Repeated calculations significantly reduce interest in mathematics. Students tend to get bored when classes are based on monotonously solving sheets and plagued sheets of exercises. What if we do some different activity with the calculator as the protagonist?`,
       },
     ]
   },
