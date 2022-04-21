3ac526386270df370510e3b0a359b1e259c2ee81
na-ir <hussain07ali@gmail.com>
2022-04-21 20:21:00 +0530

    age cal content

--- src/components/content/love-cal.tsx
+++ src/components/content/love-cal.tsx
@@ -1,19 +1,15 @@
 
 const content = {
-  title: "Love Calculator",
-  description: "Love Compatibility Calculator – Test Your Love Percentage",
+  title: "Age Calculator",
+  description: "Age calculator is an online tool with which you can find out your age using your date of birth and the current date.",
   listItems: [
     {
-      title: `Love Compatibility Calculator – Test Your Love Percentage`,
-      description: `Officia et fugiat mollit qui. Dolor elit aliqua voluptate ipsum
-    excepteur cillum consequat consectetur duis magna qui eu consequat occaecat.
-    Deserunt nisi sit.`,
+      title: `What is age calculator`,
+      description: `Age calculator is online tool to calculate age from date of birth to current date. It can also be used to calculate time difference between two dates. The output format of the time difference is in terms of years, months and days. The calculation of age or time difference does not depend on the timezone of the person as the output is the difference of the time. This age calculator is created on the basis of most common age system, so that it stays relevant for most of the people.`,
     },
     {
-      title: `Developers`,
-      description: `Pariatur consectetur laboris exercitation duis laboris.
-    Commodo duis fugiat magna fugiat et ut anim elit. Tempor aute ex qui
-    tempor tempor.`,
+      title: `How Can I Calculate Age By Using Age Calculator`,
+      description: `Date of birth (DOB) is the only necessary information you need to insert to check the age. The format would be by default, and you will be asked to enter the day, month, and year of you getting born. It will then fetch the information and calculate how many days it’s been on this planet earth. The recent features added to the age calculator are exciting because they can show you the age in terms of days, months, years, hours, or even minutes. Getting how many hours you have spent on this planet is the type of thing that will help you realize the importance of having a purpose in life.`,
     },
     {
       title: `Product owners`,
