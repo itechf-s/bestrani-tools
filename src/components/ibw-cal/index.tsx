7d663616e79ab261c5a8f273321fe5028b19a7b0
Kaunain Ahmad <kaunain.ahmad2000@gmail.com>
2022-04-27 23:48:53 +0530

    updated

--- src/components/ibw-cal/index.tsx
+++ src/components/ibw-cal/index.tsx
@@ -1,45 +1,114 @@
+import React, { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 import { tw } from 'twind';
-import Button from '@/components/button';
+import style from "@/components/style"
 
-const IdealBodyWeight = () => (
-  <div className={tw(`container`)}>
-    <h2 className={tw(`text-center pb-4 text-5xl lg:text-7xl font-bold tracking-tight text-gray-900 rounded-xl`)}>
-      Ideal Body Weight Calculator
-    </h2>
-      <div className={tw(`flex flex-col max-w-7xl py-6 sm:px-6 lg:px-8 border-1 border-red-200`)}>
-        <h4 className={tw(`font-mono text-sm uppercase text-gray-500 mb-3`)}>Gender</h4>
-        <div className={tw(`flex w-full`)}>
-          <input
-            aria-label="ages"
-            type="radio"
-            className={tw(`border border-gray-300 bg-red-100 min-w-0 w-full rounded text-gray-800 py-2 px-3 mr-2`)}
-            placeholder="Ages 2 - 80"
-          />
-        </div>
-        <h4 className={tw(`font-mono text-sm uppercase text-gray-500 mb-3`)}>Age</h4>
-        <div className={tw(`flex w-full`)}>
-          <input
-            aria-label="ages"
-            type="text"
-            className={tw(`border border-gray-300 bg-red-100 min-w-0 w-full rounded text-gray-800 py-2 px-3 mr-2`)}
-            placeholder="Ages 2 - 80"
-          />
-        </div>
-        <h4 className={tw(`font-mono text-sm uppercase text-gray-500 mb-3`)}>Height</h4>
-        <div className={tw(`flex w-full`)}>
-          <input
-            aria-label="height"
-            type="text"
-            className={tw(`border border-gray-300 bg-red-100 min-w-0 w-full rounded text-gray-800 py-2 px-3 mr-2`)}
-            placeholder="Enter your height in Cms"
-          />
-        </div>
-        <div className={tw(`flex w-full`)}>
-          <Button>Subscribe</Button>
-        </div>
-      </div>
-      
-  </div>
-);
+type Inputs = {
+  gender : string,
+  age: String,
+  height: String
+};
 
-export default IdealBodyWeight;
+type Result = { name: string, score: string, img: string, msg: string }
+const resultList: Result[] = [
+
+  { name: "", score: "0-20", img: "https://img.bestrani.com/2022/03/love-image-0-20.webp", msg: "If Elizabeth and Mr. Darcy of Pride and Prejudice had calculated their love compatibility, chances are they would have found themselves in this category! But love stories in 1813 were quite different from modern love stories. Compatibility of personalities and sometimes even professions is a MUST. If you do not have that with your crush or current partner, you sure will find it with a more warm soul who will be ready to accept as you are." },
+  { name: "", score: "21-50", img: "https://img.bestrani.com/2022/03/love-image-21-50.webp", msg: "Is your partner going hot and cold on you? Do you often feel like you are chasing a mirage? Probably, you are. Listen, your heart is not always right. Hormones play tricks all the time. If you both are looking for a fling, this number is good. But if you are looking for something meaningful, look for the real deal. Fix your love glasses because your person is probably right in front of your eyes" },
+  { name: "", score: "51-79", img: "https://img.bestrani.com/2022/03/love-image-51-79.webp", msg: "You may not agree on everything, but you both care for each other. Discussions are intense, so are your parting hugs. Compatibility may not be 100%, but love sure is in the air. If there’s something that is bothering you about your partner, sit down, and have a conversation. Address the issues before they get out of hand. Having healthy boundaries is good for both of you in the long run. Also, don’t forget to empathize with your partner’s difference in opinion and outlook." },
+  { name: "", score: "80-100", img: "https://img.bestrani.com/2022/03/love-image-80-100.webp", msg: "Everything else fades when you’ve found “the one.” Your love and compatibility can make the gods jealous. From your choice of food, cities to travel, music to dance to, and weird hobbies – you both mirror each other, bringing out the best of your personalities and qualities. Yes, there are occasional arguments, but you both soon find solutions and go about your day. What you have is precious (*touchwood). Nurture and preserve. Time may test your bond, but you sure will overcome it." },
+]
+
+export default function IdealBodyWeight() {
+  const { register, handleSubmit, formState: { errors } } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = data => {
+    console.log(data.gender);
+    
+  }
+  const [result, setResult] = useState<Result>({ name: "", score: "", img: "", msg: "" });
+
+  return (
+    <div className={tw(`container`)}>
+      <form onSubmit={handleSubmit(onSubmit)} className={tw`${style.twForm}`} >
+        <label className={tw`${style.twLabel}`}>Gender</label>
+        <br />
+      <input {...register("gender", { required: true })} type="radio" id="male" value="Male" className={tw`${style.twRadioBtn}`} />
+      <label htmlFor="male" className={tw`${style.twRadioLabel}`}>Male</label>
+      <input {...register("gender", { required: true })} type="radio" id="female" value="Female" className={tw`${style.twRadioBtn}`} />
+      <label htmlFor="female" className={tw`${style.twRadioLabel}`}>Female</label>
+
+      <br />
+        <label className={tw`${style.twLabel}`}>System</label>
+        <br />
+        <input {...register("gender", { required: true })} type="radio" id="Metric" value="Metric" className={tw`${style.twRadioBtn}`} />
+      <label htmlFor="Metric" className={tw`${style.twRadioLabel}`}>Metric</label>
+      <input {...register("gender", { required: true })} type="radio" id="Imperial" value="Imperial" className={tw`${style.twRadioBtn}`} />
+      <label htmlFor="Imperial" className={tw`${style.twRadioLabel}`}>Imperial</label>
+
+        <br />
+        <label className={tw`${style.twLabel}`}>Age</label>
+        <br />
+        <input type="text" placeholder="Your Age 2-85" {...register("age", { required: true })}
+          className={tw`${style.twInputText}`} />
+        {errors.age && <span className={tw`${style.twErrorMsg}`}>Age is required</span>}
+        <br />
+        <label className={tw`${style.twLabel}`}>Your Height</label>
+        <br />
+        <input type="text" placeholder="Your Height in Cms" {...register("height", { required: true })}
+          className={tw`${style.twInputText}`}
+        />
+        {errors.height && <span className={tw`${style.twErrorMsg}`}>Height is required</span>}
+
+        <br />
+        <button className={tw`${style.twInputBtn}`}>
+          Calculate
+        </button>
+      </form>
+      {result.name &&
+        <>
+          <div className={tw`${style.twResultT}`}>Results</div>
+          <div className={tw`${style.twResultBlock}`}>
+            <div className={tw`${style.twResultLabel}`}>{result.name}</div>
+            <div className={tw`${style.twResultText}`}>{result.score} %</div>
+            <div><img src={result.img} alt="Love calculator" title="Love calculator" height={100} width={100} /></div>
+            <div className={tw`${style.twResultLabel}`}>{result.msg}</div>
+          </div>
+        </>
+      }
+    </div>
+  );
+}
+
+
+function convertStrToNumberLogic(str: String) {
+  str = str.replace(/ /g, '').toLowerCase();
+  var i = 0;
+  var num = '';
+  while (str.length) {
+    var c = str.charAt(i);
+    var pattern = new RegExp(c, 'g');
+    num += str.split(c).length - 1;
+    str = str.replace(pattern, '');
+  }
+  var per = calculateLovePercentage(num);
+  return per;
+}
+function calculateLovePercentage(str: String) {
+  var per = '';
+  while (str.length) {
+    if (str.length == 1) {
+      var f = str.charAt(0);
+      per += f;
+      str = '';
+    } else {
+      var f = str.charAt(0);
+      var l = str.charAt(str.length - 1);
+      per += +f + +l;
+      var position = 1;
+      str = str.substring(0, position - 1) + str.substring(position, str.length);
+      position = str.length;
+      str = str.substring(0, position - 1) + str.substring(position, str.length);
+    }
+  }
+  if (per.length > 2) per = calculateLovePercentage(per);
+  return per;
+}
--- src/components/style/index.tsx
+++ src/components/style/index.tsx
@@ -3,6 +3,8 @@ import { apply } from "twind";
 const style = {
     twForm: apply(`border-2 border-gray-300 bg-gray-200 rounded-lg shadow-lg max-w-sm my-8 p-8 m-auto space-y-2`),
     twInputBtn: apply(`max-w-sm m-auto items-center py-2 px-4 border-2 rounded text-uppercase font-bold rounded-lg bg-yellow-400 text-black shadow-md hover:bg-pink-100`),
+    twRadioBtn: apply(`w-6 h-6 inline-block mr-1 ml-2 border border-red-300`),
+    twRadioLabel: apply(`pl-1 pr-2 text-xl text-blue-600 font-medium hover:text-blue-800 hover:font-bold`),
     twInputText: apply(`w-full py-2 pl-3 pr-4 rounded-lg shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-2 focus-visible:border-indigo-500 hover:bg-pink-100`),
     twLabel: apply(`p-2 text-lg text-black font-bold rounded-lg hover:bg-pink-100`),
     twErrorMsg: apply(`text-red-600 py-2`),
