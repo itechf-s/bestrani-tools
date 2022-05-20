c6069e997fe2b25db3ca304bf383f78dd13d89f3
na-ir <hussain07ali@gmail.com>
2022-05-20 16:35:20 +0530

    Protein calculator-content

--- src/components/content/protein-ical.tsx
+++ src/components/content/protein-ical.tsx
@@ -1,13 +1,13 @@
 
 const content = {
   meta: {
-    title: `Proteen Age Calculator – Calculate Your Age In Various Stage Of Life`,
-    description: `The age calculator is used to determine the interval between the specified two days. This calculator is based on the most common age system.`,
-    keyword: `protein-intake-calculator Calculator`,
+    title: `Protein Calculator – How To Calculate Protein Intake`,
+    description: `This Free Protein Calculator Estimates The Amount Of Protein A Person Needs Each Day To Remain Healthy Based On Certain Averages And Recommendations.`,
+    keyword: `Daily Protein Intake Calculator`,
     url: `https://bestrani.com/tools/protein-intake-calculator/`,
     published_time: `2022-04-24T08:30:00+05:30`,
     modified_time: `2022-04-24T08:30:00+05:30`,
-    tags: [`age calculator`, `birth day calculator`, `date calculator`],
+    tags: [`Protein Calculator`, `Protein Calculator In Food`, `Protein Calculator For Weight Loss`,`Daily Protein Intake Calculator`],
     images: [`https://img.bestrani.com/2022/03/age-calculator.webp`],
     author: `Iffat Zia`
   },
@@ -34,55 +34,168 @@ const content = {
     description: `Here are some of the most recent questions asked by the users, which we decided to answer briefly.`,
     faqList: [
       {
-        questionName: `How Does An Age Calculator Work?`,
-        acceptedAnswerText: `It works on a mathematical formula, which requires input in the form of date of birth (DOB), and it will calculate the number of days based upon the configurations you have set on it.`
+        questionName: `How do I calculate how much protein I need?`,
+        acceptedAnswerText: `<p>Calculate protein requirement by weight.Dividing your weight into pounds by 20 and multiplying it by seven. You need just a little more than seven grams of protein for every 20 pounds of your body weight. Thus, for someone weighing 200 pounds, the protein requirement will be 70 grams each day.</p>`
       },
       {
-        questionName: `Does the age calculator show age in hours, minutes?`,
-        acceptedAnswerText: `YES. It can calculate the number of days, minutes you have spent since you’re born on this planet and gives you exact data about it.`
+        questionName: `How do I calculate how much protein I need in kg?`,
+        acceptedAnswerText: `<ul>
+        <li> Weight in pounds divided by 2.2 = weight in kg.</li>
+        <li> Determine your protein number; Mildly Active = 0.8, Regularly Active = 1.5, Heavily Active = 1.8.</li>
+        <li> Weight in kg x 0.8-1.8 gm/kg = protein gm.
+        </li>
+        </ul>`
       },
       {
-        questionName: `Can it check the age of another person?`,
-        acceptedAnswerText: `YES, you need to put the DOB information, and it will calculate the age of that person accordingly.`
+        questionName: `What is a gram of protein?`,
+        acceptedAnswerText: `In nutrition science, “grams of protein” refers to the number of grams of the macronutrient protein, not the number of grams of a protein-containing food like meat or eggs. An 8-ounce serving of beef weighs 226 grams but only contains 61 grams of protein.`
       },
       {
-        questionName: `Can I save the information shown on the age calculator?`,
-        acceptedAnswerText: `Yes, you can get the required information in the default format. If you want to save it for later use, we recommend taking a picture of it or screenshot the desktop screen results. You can share the results directly with your friends by clicking on the share option also.`
+        questionName: `How much protein is too much?`,
+        acceptedAnswerText: `Most research indicates that eating more than 2 g per kg of body weight daily of protein for a long time can cause health problems. Symptoms associated with too much protein include: intestinal discomfort and indigestion. dehydration.`
+      },
+      {
+        questionName: `How do you know if your body needs more protein?`,
+        acceptedAnswerText: `Swelling. One of the most common signs that you're not getting enough protein is swelling (also called edema), especially in your abdomen, legs, feet, and hands. A possible explanation: The proteins that circulate in your blood -- albumin, in particular -- help keep fluid from building up in your tissues.`
       }
     ]
   },
   listItems: [
     {
-      title: `What is age calculator`,
-      description: `<p>Age calculator is online tool to calculate age from date of birth to current date. It can also be used to calculate time difference between two dates.</p>
-      <p>The output format of the time difference is in terms of years, months and days. The calculation of age or time difference does not depend on the timezone of the person as the output is the difference of the time.</p>
-      <p>This age calculator is created on the basis of most common age system, so that it stays relevant for most of the people.</p>`,
+      title: `What Are Proteins?`,
+      description: `<p>Proteins are the building blocks and are made up of amino acids. Our muscles, skin, bones, tendons, enzymes, digestive juices all are made of protein.</p>
+      <p>Besides supporting growth and development, proteins also provide 4 calories of energy (through oxidation of 1 g of protein).</p>`,
     },
     {
-      title: `How Can I Calculate Age By Using Age Calculator`,
-      description: `<p>Date of birth (DOB) is the only necessary information you need to insert to check the age. The format would be by default, and you will be asked to enter the day, month, and year of you getting born. </p>
-      <p>It will then fetch the information and calculate how many days it’s been on this planet earth. </p><p>The recent features added to the age calculator are exciting because they can show you the age in terms of days, months, years, hours, or even minutes.</p>
-      <p>Getting how many hours you have spent on this planet is the type of thing that will help you realize the importance of having a purpose in life.</p>`,
-    },
-    {
-      title: `How to Use`,
-      description: `<p>Enter the DOB information in month/date/year.
-      </p><p>Then enter date as on it is default set as current date.</p><p>
-      </p><p>Then click on the Calculate button.</p><p>
-      Wait a few seconds and get the result.</p>`,
-    },
-    {
-      title: `How does this age calculator work?`,
-      description: `<p>India, as a country, is an amalgamation of cultures where various types of traditional calendars are followed. Prominent among them include the Saka Samvat, Vikram Samvat, and the Hijri calendar. But, when it comes to official purposes, the Gregorian calendar is the calendar used in India as well as various countries around the world.
-      </p><p>
-      This calculator also uses the Gregorian calendar to make calculations. A year is divided into 365 days in this calendar, except for a leap year, which has 366 days. The total number of days in a calendar year is categorized into 12 months of varying lengths. Each month either has 30 or 31 days, except February which has 28 days in a normal year and 29 days in a leap year.
-      </p><p>
-      The system used by this calculator is the most commonly used age system. In this calculator, a person’s age starts from zero and the person turns one year old only after 12 months have passed. Similarly, in any type of age calculation here, the age is increased by one year after the completion of 12 months from the birth date/start date.<p>
+      title: `How To Calculate Protein Intake?`,
+      description: `<p>Protein is an important nutrient for bodily functions. This calculator helps to determine your total protein intake as per your calorie requirement, age, and physical activity which is important to meet the demand and help to regulate satiety level.</p>
       `,
     },
     {
-      title: `What role does time zone play in the process of age calculation?`,
-      description: `<p>Though the time zone doesn’t play a significant role in age calculation, this calculator can detect the user's time zone and display the required age accordingly.<p>`,
+      title: `How Much Protein Do We Need?`,
+      description: `<p>The amount of protein required depends on the age, sex, and physical activity of the person. The current International Recommended Dietary Allowance (RDA) for protein is 0.8 g/kg body weight, irrespective of age.</p>
+      <p>This is subject to change based on physical activity and needs to be distributed as nearly 25-30 g of high-quality protein in every meal (considering 3 meals).</p>
+      <p>As per the American Dietetic Association, the protein requirement depends on the age and physical activity of a person.</p>
+      <ul>
+      <li> No Exercise to Low Exercise: 0.8-1 g/body weight</li>
+      <li> Little to Moderate Exercise: 1-1.8 g/body weight</li>
+      <li> Heavy to Extra Exercise (athletes or sportspersons): 2g/kg body weight</li>
+      </ul>
+      `,
+    },
+    {
+      title: `Is Too Much Protein Bad?`,
+      description: `<p>Excess protein intake may have toxic effects on the body. Ammonia is the by-product of protein metabolism. Protein intake beyond RDA produces more ammonia, which is converted to urea. Excess urea production burdens the kidneys.</p>`,
+    },
+    {
+      title: `Should I Increase My Protein Intake To Lose Weight?`,
+      description: `<p>Protein can help you lose weight when combined with other macronutrients in a balanced manner. Adequate protein intake as per the weight and physical activity increases satiety, has a thermic effect that is more when compared to carbs and fat, and boosts the metabolic rate.</p>`,
+     },
+    {
+      title: `How To Calculate Protein Intake?`,
+      description: ` <ol>
+      <li> The protein requirement depends on the age and activity level of a person.</li>
+      <li> Calculate your body weight on a digital weighing machine. For conversion from pounds to kg, multiply pounds by 2.2 ( pounds*2.2= weight in kg).</li>
+      <li>Analyze your physical activity. If you are involved in 10 hours of desk exercise with no physical activity, multiply 0.8 g with your body weight to understand how much protein is required per kg of your body weight to maintain muscle mass.</li>
+      </ol>`,    
+    },
+
+    {
+      title: `Recommended Dietary Allowances Of Protein`,
+      description: `<p>The RDA changes with age and is given in the following table:</p>
+    <table class="border-collapse border border-slate-400">
+    <thead><tr>
+    <th class="border border-slate-300 ...">AGE</th>
+    <th class="border border-slate-300 ...">PROTEIN REQUIREMENT (G/DAY)</th>
+  </tr>
+</thead>
+<thead>
+    <tr>
+      <th class="border border-slate-300 ..."></th>
+      <th class="border border-slate-300 ...">CHILD</th>
+      <th class="border border-slate-300 ...">MALE</th>
+      <th class="border border-slate-300 ...">FEMALE</th>
+
+    </tr>
+  </thead>
+  <tr>
+  <td class="border border-slate-300 ...">1-3 years</td>
+  <td class="border border-slate-300 ...">13</td>
+  <td class="border border-slate-300 ...">-</td>
+  <td class="border border-slate-300 ...">-</td>
+</tr>
+<tr>
+<td class="border border-slate-300 ...">4-8 years</td>
+<td class="border border-slate-300 ...">-</td>
+<td class="border border-slate-300 ...">19</td>
+<td class="border border-slate-300 ...">19</td>
+</tr>
+<tr>
+<td class="border border-slate-300 ...">9-13 years</td>
+<td class="border border-slate-300 ...">-</td>
+<td class="border border-slate-300 ...">34</td>
+<td class="border border-slate-300 ...">34</td>
+</tr>
+<tr>
+<td class="border border-slate-300 ...">14-18 years</td>
+<td class="border border-slate-300 ...">-</td>
+<td class="border border-slate-300 ...">46</td>
+<td class="border border-slate-300 ...">52</td>
+</tr>
+<tr>
+<td class="border border-slate-300 ...">19-30 years</td>
+<td class="border border-slate-300 ...">-</td>
+<td class="border border-slate-300 ...">46</td>
+<td class="border border-slate-300 ...">56</td>
+</tr>
+<tr>
+<td class="border border-slate-300 ...">31-50 years</td>
+<td class="border border-slate-300 ...">-</td>
+<td class="border border-slate-300 ...">46</td>
+<td class="border border-slate-300 ...">56</td>
+</tr>
+<tr>
+<td class="border border-slate-300 ...">51+ years</td>
+<td class="border border-slate-300 ...">-</td>
+<td class="border border-slate-300 ...">46</td>
+<td class="border border-slate-300 ...">56</td>
+</tr>
+</table>
+  <p>Pregnancy and lactation are anabolic phases that require extra protein intake.
+  </p>
+  <p>Extra Protein Requirements For Pregnancy And Lactation As Per Revised ICMR, 2010
+  </p><br>
+ 
+
+<table class="border-collapse border border-slate-400">
+  <thead><tr>
+  <th></th>
+  <th class="border border-slate-300 ...">SAFE INTAKE(GRAMS / DAY)</th>
+  <th class="border border-slate-300 ...">ADDITIONAL ENERGY REQUIREMENT(KJ/DAY)</th>
+</tr>
+<tr>
+  <td class="border border-slate-300 ...">Pregnancy trimester 1</td>
+  <td class="border border-slate-300 ...">1</td>
+  <td class="border border-slate-300 ...">375</td>
+</tr>
+<tr>
+  <td class="border border-slate-300 ...">Pregnancy trimester 2</td>
+  <td class="border border-slate-300 ...">10</td>
+  <td class="border border-slate-300 ...">1200</td>
+</tr>
+<tr>
+  <td class="border border-slate-300 ...">Pregnancy trimester 3</td>
+  <td class="border border-slate-300 ...">31</td>
+  <td class="border border-slate-300 ...">1,950</td>
+</tr>
+<tr>
+  <td class="border border-slate-300 ...">Lactation After 6 months</td>
+  <td class="border border-slate-300 ...">13</td>
+  <td class="border border-slate-300 ...">1,925</td>
+</tr>
+</table>
+
+  `,
     },
   ],
   embed:{
